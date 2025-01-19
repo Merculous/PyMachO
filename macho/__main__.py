3ec52e8b3b9684b8f4ff2a288ffa6efc5966b32c
Merculous <24739823+Merculous@users.noreply.github.com>
2025-01-18 23:15:50 -0600

    Remove stuff from __main__ too.

--- macho/__main__.py
+++ macho/__main__.py
@@ -1,27 +1,10 @@
 
 from argparse import ArgumentParser
 
-from .io import readBinaryFileAtPath
-from .macho import MachO
-
 
 def main() -> None:
     parser = ArgumentParser()
-
-    parser.add_argument('-i', nargs=1)
-    parser.add_argument('-kextNames', action='store_true')
-
     args = parser.parse_args()
-
-    if not args.i or not args.kextNames:
-        return parser.print_help()
-
-    if args.i:
-        input_data = readBinaryFileAtPath(args.i[0])
-        macho = MachO(input_data)
-
-        if args.kextNames:
-            return macho.printKextNames()
 
 
 if __name__ == '__main__':
